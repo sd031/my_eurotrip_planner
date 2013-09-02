a58e2888f681ac362c227143eac0bc10e4515015
Maria Emilia Torino <emilia.torino@west.cmu.edu>
2013-09-01 21:02:39 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyEurotripPlanner::Application.config.secret_token = 'fc01d047e3c1364b035486e0d4533c5f062f368663a26a05cc76ec4b44734d810bbe0253b9fec4ad245b1940d3fd8b652eba1036b3609a27faf955a9d7d6cf3e'
