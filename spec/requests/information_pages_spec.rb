1b0597bb7a6817f2edb212d0de7f5192b465e0aa
Maria Emilia Torino <emilia.torino@west.cmu.edu>
2013-09-06 22:34:31 -0700

    Refactoring unit tests

--- spec/requests/information_pages_spec.rb
+++ spec/requests/information_pages_spec.rb
@@ -3,19 +3,29 @@ require 'spec_helper'
 describe "InformationPages" do
   subject { page }
 
+  shared_examples_for "all information pages" do
+    it { should have_title(full_title(page_title)) }
+    it { should have_content(content)}
+  end
+
   describe "About page" do
     before { visit about_path }
 
-    it {should have_content('We are a team of young people as excited as you about traveling to Europe.
+    let (:content) { 'We are a team of young people as excited as you about traveling to Europe.
 We have been building a nice community with other travelers and Europe natives that help us enrich this site and provide you with the best alternatives to make your trip come true!
-You are invited to join us either if you are planning your trip or you have just come back from it.... or if you just love this idea, please ')}
+You are invited to join us either if you are planning your trip or you have just come back from it.... or if you just love this idea, please '}
+    let(:page_title) { "About" }
+
+    it_should_behave_like "all information pages"
+
+    #TODO: Improve this test to use the click_link method instead.
     it {should have_link('contact us!', href:contactus_path) }
-    it {should have_title(full_title("About"))}
   end
 
   describe "Contact Us page" do
     before {visit contactus_path}
-    it {should have_content("Please, leave us your comment!")}
-    it {should have_title(full_title("Contact Us")) }
+    let (:content) {"Please, leave us your comment!"}
+    let(:page_title) { "Contact Us" }
+    it_should_behave_like "all information pages"
     end
 end
--- spec/requests/trip_pages_spec.rb
+++ spec/requests/trip_pages_spec.rb
@@ -4,22 +4,32 @@ describe "Trip pages" do
 
   subject { page }
 
+  shared_examples_for "all trip pages" do
+    it { should have_title(full_title(page_title)) }
+    it { should have_content(content)}
+    end
+
   describe "Home page" do
     before { visit home_path }
-    it { should have_content('Do you love traveling and planing your trips in advance?
+    let (:content) { 'Do you love traveling and planing your trips in advance?
     Are you a middle age person planning your backpacker dream trip to Europe?
     Do you need some help on planning a trip adapted just to your preferences?
-    Are you dizzy with the amount of information you have found about it?')}
-    it { should have_title(full_title('Home')) }
+    Are you dizzy with the amount of information you have found about it?'}
+    let (:page_title) { 'Home'}
+
+    it_should_behave_like "all trip pages"
     it { should have_link("Show me my itinerary", href: itinerary_path)}
   end
 
   describe "Itinerary page" do
     before { visit itinerary_path }
 
-    it { should have_content('Awesome! We have found a nice option just for you! Actually this is the preferred
- route most people followed!') }
-    it { should have_title(full_title('Itinerary')) }
+    let (:content) {'Awesome! We have found a nice option just for you! Actually this is the preferred
+ route most people followed!' }
+    let (:page_title) {'Itinerary' }
+    it_should_behave_like "all trip pages"
   end
+
+  # add tests for  it { should have_selector('h1', text: 'Contact') }
 end
 
